1b1cf9df2c8768764e0d0067076396754ddba94b
Ray Qiu <ray@mistnet.io>
2018-12-06 14:56:44 -0800

    fixed according to library path change

--- examples/main.go
+++ examples/main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"github.com/rayqiu/binheap"
 	"fmt"
+	"github.com/rayqiu/binheap"
 	"math/rand"
 	"sort"
 	"time"
 )
 
-func isSameSlice(a, b []int, reversed bool) bool {
+func reverseSlice(a []int) {
+	for i := 0; i < len(a)/2; i++ {
+		j := len(a) - 1 - i
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func isSameSlice(a, b []int) bool {
 	i := len(a)
 	j := len(b)
 	if i != j {
 		return false
 	}
 	for k, v := range a {
-		if reversed {
-			if v != b[j-k-1] {
-				return false
-			}
-		} else {
-			if v != b[k] {
-				return false
-			}
+		if v != b[k] {
+			return false
 		}
 	}
 	return true
@@ -31,7 +32,7 @@ func isSameSlice(a, b []int, reversed bool) bool {
 func main() {
 	arr := []int{}
 	isMax := true
-	h := binheap2.NewBinaryIntHeap(arr, isMax)
+	h := binheap.NewBinaryIntHeap(arr, isMax)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	j := r.Intn(1000)
 	for i := 0; i < j; i++ {
@@ -46,11 +47,37 @@ func main() {
 		}
 	}
 	d := h.PopAll()
-	sort.Ints(arr)
-	if isSameSlice(d, arr, isMax) {
+  sort.Ints(arr)
+  reverseSlice(arr)
+	if isSameSlice(d, arr) {
 		fmt.Println("Test successful!")
+		fmt.Println("Sorted:", arr)
+	} else {
+		fmt.Println("Test failed!")
 		fmt.Println("Binheap sorted:", d)
 		fmt.Println("Golang library sorted:", arr)
+	}
+	arr = []int{}
+	isMax = false
+	h = binheap.NewBinaryIntHeap(arr, isMax)
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	j = r.Intn(10000)
+	for i := 0; i < j; i++ {
+		m := r.Int()
+		n := r.Intn(2)
+		if n == 0 {
+			h.Insert(-m)
+			arr = append(arr, -m)
+		} else {
+			h.Insert(m)
+			arr = append(arr, m)
+		}
+	}
+	d = h.PopAll()
+	sort.Ints(arr)
+	if isSameSlice(d, arr) {
+		fmt.Println("Test successful!")
+		fmt.Println("Sorted:", arr)
 	} else {
 		fmt.Println("Test failed!")
 		fmt.Println("Binheap sorted:", d)
